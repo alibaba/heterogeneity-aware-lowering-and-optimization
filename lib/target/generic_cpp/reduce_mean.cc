5defc27ef88a218c876348502cfd54ad6f74432c
Weiming Zhao <weimingz@alibaba-inc.com>
2020-11-30 23:23:58 +0000

    [CG] Emit more math ops
    Emit odla_Neg, odla_Not, odla_reduce_xxx, odla_cast etc.

--- lib/target/generic_cpp/reduce_mean.cc
+++ lib/target/generic_cpp/reduce_mean.cc
@@ -19,7 +19,9 @@
 
 namespace halo {
 
-void GenericCXXCodeGen::RunOnInstruction(ReduceMeanInst* inst) {
+void GenericCXXCodeGen::RunOnReductionInstruction(
+    Instruction* inst, const std::vector<int32_t>& axis_attr, bool keep_dims,
+    const char* odla_func_name) {
   const Def& input = inst->GetOperand(0);
 
   CXXValue op0 = ir_mapping_[input];
@@ -27,13 +29,51 @@ void GenericCXXCodeGen::RunOnInstruction(ReduceMeanInst* inst) {
 
   CXXValue ret(inst->GetName(), op0.type);
   std::vector<uint32_t> axis;
-  for (auto x : inst->GetAxis()) {
+
+  size_t dims = input.GetType().GetNumOfDims();
+  axis.reserve(dims);
+  for (auto x : axis_attr) {
     axis.push_back(x);
   }
+  if (axis.empty()) {
+    for (size_t i = 0; i < dims; ++i) {
+      axis.push_back(i);
+    }
+  }
 
-  EmitODLACall(ret, "odla_ReduceMean", op0, axis.size(), axis,
-               inst->GetKeepDims(), EmitShape(ret_type));
+  EmitODLACall(ret, odla_func_name, op0, axis.size(), axis, keep_dims,
+               EmitShape(ret_type));
   ir_mapping_[*inst] = ret;
+}
+
+void GenericCXXCodeGen::RunOnInstruction(ReduceMeanInst* inst) {
+  RunOnReductionInstruction(inst, inst->GetAxis(), inst->GetKeepDims(),
+                            "odla_ReduceMean");
+}
+
+void GenericCXXCodeGen::RunOnInstruction(ReduceL1Inst* inst) {
+  RunOnReductionInstruction(inst, inst->GetAxis(), inst->GetKeepDims(),
+                            "odla_ReduceL1");
+}
+
+void GenericCXXCodeGen::RunOnInstruction(ReduceL2Inst* inst) {
+  RunOnReductionInstruction(inst, inst->GetAxis(), inst->GetKeepDims(),
+                            "odla_ReduceL2");
+}
+
+void GenericCXXCodeGen::RunOnInstruction(ReduceLogSumInst* inst) {
+  RunOnReductionInstruction(inst, inst->GetAxis(), inst->GetKeepDims(),
+                            "odla_ReduceLogSum");
+}
+
+void GenericCXXCodeGen::RunOnInstruction(ReduceLogSumExpInst* inst) {
+  RunOnReductionInstruction(inst, inst->GetAxis(), inst->GetKeepDims(),
+                            "odla_ReduceLogSumExp");
+}
+
+void GenericCXXCodeGen::RunOnInstruction(ReduceSumSquareInst* inst) {
+  RunOnReductionInstruction(inst, inst->GetAxis(), inst->GetKeepDims(),
+                            "odla_ReduceSumSquare");
 }
 
 } // namespace halo
