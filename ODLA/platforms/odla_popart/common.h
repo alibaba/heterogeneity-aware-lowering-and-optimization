83885c8baf213a647f806192279d5eedcb439532
yanwei-gr <64010848+yanwei-gr@users.noreply.github.com>
2020-12-02 18:25:36 +0800

    add odla_popart code (#44)
    * add odla_popart code
    
    * add library check for poplar lib. if didn't found skip the popart build
    
    * clang formatted . and update onnx version to use the same protobuf version of halo
    
    * add odla_popart code to support ipu
    
    * use compiled so file instead of include onnx files
    
    * clang formatted
    
    * format files
    
    * fix format
    
    * add odla_popart code
    
    * add library check for poplar lib. if didn't found skip the popart build
    
    * clang formatted . and update onnx version to use the same protobuf version of halo
    
    * use compiled so file instead of include onnx files
    
    * clang formatted
    
    * format files
    
    * add odla_popart code
    
    * add library check for poplar lib. if didn't found skip the popart build
    
    * clang formatted . and update onnx version to use the same protobuf version of halo
    
    * use compiled so file instead of include onnx files
    
    * clang formatted
    
    * format files
    
    * add odla_popart code
    
    * clang formatted . and update onnx version to use the same protobuf version of halo
    
    * use compiled so file instead of include onnx files
    
    * clang formatted
    
    * format files
    
    * fix lint error
    
    * fix linter error
    
    Co-authored-by: Weiming Zhao <wmzhao.code@gmail.com>

---
+++ ODLA/platforms/odla_popart/common.h
@@ -0,0 +1,38 @@
+//
+// Copyright (c) 2020 Graphcore Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
+
+#ifndef ODLA_POPART_COMMON_H_
+#define ODLA_POPART_COMMON_H_
+
+#include <ODLA/odla.h>
+
+#include <memory>
+#include <popart/iarray.hpp>
+#include <popart/names.hpp>
+#include <popart/tensorinfo.hpp>
+#include <vector>
+
+popart::DataType GetPopartType(odla_value_type type);
+popart::Shape GetPopartShape(odla_value_shape shape);
+std::string&& GetDirectionName(odla_rnn_direction direction);
+std::string&& GetTypeName(odla_element_type element_type);
+std::string&& GetResizeInterpolationModeName(odla_interpolation_mode mode);
+odla_element_type GetOdlaType(popart::DataType type);
+
+std::unique_ptr<popart::IArray> MakeNDArrayWrapper(const odla_void* data_ptr,
+                                                   popart::DataType data_type,
+                                                   std::vector<int64_t> shape);
+#endif
