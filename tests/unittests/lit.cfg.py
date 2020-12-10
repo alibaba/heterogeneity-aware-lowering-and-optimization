8899f5230395a5088d4998a592b4648c3a4fbb2c
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2020-12-10 13:52:15 -0800

    [Unit tests] remove duplicated environment variable setting

--- tests/unittests/lit.cfg.py
+++ tests/unittests/lit.cfg.py
@@ -14,10 +14,6 @@ config.is_early = True
 # Setup source root.
 config.test_source_root = os.path.join(config.halo_src_dir, 'tests/unittests')
 
-config.environment['HALO_BUILD_DIR'] = config.halo_build_dir
-config.environment['HALO_SRC_DIR'] = config.halo_src_dir
-config.environment['HALO_UNITTESTS_PATH'] = config.halo_unittests_dir
-
 config.substitutions.append(('%src_dir', config.halo_src_dir))
 config.substitutions.append(('%build_dir', config.halo_build_dir))
 
