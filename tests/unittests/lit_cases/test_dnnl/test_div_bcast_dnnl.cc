3e750ff8311b389feeaa0064ce3dec2aa8763c13
shenghui.liu <shliusdu@qq.com>
2020-12-28 12:38:01 +0000

    enable tests

--- tests/unittests/lit_cases/test_dnnl/test_div_bcast_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_div_bcast_dnnl.cc
@@ -23,5 +23,5 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_div_bcast | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_div_bcast_dnnl.cc.tmp.main.cc.in"
--- tests/unittests/lit_cases/test_dnnl/test_sub_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_sub_dnnl.cc
@@ -23,5 +23,5 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_sub | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_sub_dnnl.cc.tmp.main.cc.in"
