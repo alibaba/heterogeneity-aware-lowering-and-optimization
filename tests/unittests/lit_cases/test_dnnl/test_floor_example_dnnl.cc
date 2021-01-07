dcca3a88da3b940573b51cf0cc5c86561cc4f5db
shenghui.liu <shliusdu@qq.com>
2021-01-07 21:12:19 +0000

    1. bf16 support for nms/topk ops 2. add new function erf_bf16_func 3. fix avx512 computation bugs 4. refactor gather_func code

--- tests/unittests/lit_cases/test_dnnl/test_floor_example_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_floor_example_dnnl.cc
@@ -23,5 +23,5 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_floor_example | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_floor_example_dnnl.cc.tmp.main.cc.in"
