aa7fb86daa3d93446787300a6a601b34943fe120
Weiming Zhao <weimingz@alibaba-inc.com>
2021-01-18 05:18:08 +0000

    [ODLA/TRT] Add more logical and math ops

--- tests/unittests/lit_cases/test_tensorrt/test_reduce_sum_square_do_not_keepdims_example_tensorrt.cc
+++ tests/unittests/lit_cases/test_tensorrt/test_reduce_sum_square_do_not_keepdims_example_tensorrt.cc
@@ -25,5 +25,5 @@
 // RUN: %t_tensorrt.exe 0.0001 0 tensorrt %data_path/test_reduce_sum_square_do_not_keepdims_example | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_reduce_sum_square_do_not_keepdims_example_tensorrt.cc.tmp.main.cc.in"
