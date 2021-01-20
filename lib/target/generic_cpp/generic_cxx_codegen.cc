048132f06176326339d8ca0db84df0926ad81455
Weiming Zhao <weimingz@alibaba-inc.com>
2021-01-20 00:50:46 +0000

    [CodeGen] Add func to emit multiple types and ids

--- lib/target/generic_cpp/generic_cxx_codegen.cc
+++ lib/target/generic_cpp/generic_cxx_codegen.cc
@@ -820,6 +820,11 @@ void GenericCXXCodeGen::EmitODLAArgs(const std::vector<uint32_t>& arg) {
   os_ << '{' << Join(arg) << '}';
 }
 
+void GenericCXXCodeGen::EmitODLAArgs(const std::vector<float>& arg) {
+  os_ << "(const odla_float32[])";
+  os_ << '{' << Join(arg) << '}';
+}
+
 void GenericCXXCodeGen::EmitODLAArgs(const std::vector<CXXValue>& arg) {
   os_ << "(odla_values){.size = " << arg.size() << ", .values = {";
   for (const auto& v : arg) {
@@ -845,6 +850,25 @@ void GenericCXXCodeGen::EmitODLAArgs(const DataFormat& arg) {
   }
   os_ << "ODLA_";
   os_ << (arg == DataFormat::NHWC ? "CHANNELS_LAST" : "CHANNELS_FIRST");
+}
+
+void GenericCXXCodeGen::EmitODLAArgs(const std::vector<halo::Type>& arg) {
+  os_ << "(odla_types){.size = " << arg.size() << ", .types = {";
+  for (const auto& v : arg) {
+    EmitODLAArgs(v);
+    os_ << ", ";
+  }
+  os_ << "}}";
+}
+
+void GenericCXXCodeGen::EmitODLAArgs(const std::vector<std::string>& arg) {
+  os_ << "(const odla_char* const[])"
+      << "{ ";
+  for (const auto& v : arg) {
+    os_ << "\"" << v << "\"";
+    os_ << ", ";
+  }
+  os_ << '}';
 }
 
 } // namespace halo
