44e6818b9e9b5617b1c9fdf78c10f8ca18654132
xuhongyao.xhy <xuhongyao.xhy@alibaba-inc.com>
2021-02-01 00:21:27 -0800

    change the result of squeezenet_1_0 on dnnl

--- models/vision/classification/squeezenet/run_squeezenet_1_0.sh
+++ models/vision/classification/squeezenet/run_squeezenet_1_0.sh
@@ -17,15 +17,18 @@ $curr_dir/../../get_images.sh $image_dir
 if [[ $TEST_WITH_GPU -eq 1 ]]; then
   echo "======== Testing with ODLA TensorRT ========"
   python3 $curr_dir/../../invoke_halo.py --model $model_file --label-file $curr_dir/../1000_labels.txt --image-dir $image_dir --odla tensorrt
-# RUN: FileCheck --input-file %test_temp_dir/squeezenet_1_0_tensorrt.txt %s
+# RUN: FileCheck --input-file %test_temp_dir/squeezenet_1_0_tensorrt.txt --check-prefix CHECK-TENSORRT %s
+# CHECK-TENSORRT: dog.jpg ==> "Samoyed, Samoyede",
+# CHECK-TENSORRT: sport.jpg ==> "ski",
+# CHECK-TENSORRT: food.jpg ==> "ice cream, icecream",
+# CHECK-TENSORRT: plane.jpg ==> "airliner",
 fi
 
 # Using HALO to compile and run inference with ODLA DNNL
 echo "======== Testing with ODLA DNNL (NHWC) ========"
 python3 $curr_dir/../../invoke_halo.py --model $model_file --label-file $curr_dir/../1000_labels.txt --image-dir $image_dir --odla dnnl --convert-layout-to=nhwc
-# RUN: FileCheck --input-file %test_temp_dir/squeezenet_1_0_dnnl.txt %s
-
-# CHECK: dog.jpg ==> "Samoyed, Samoyede",
-# CHECK-NEXT: sport.jpg ==> "ski",
-# CHECK-NEXT: food.jpg ==> "ice cream, icecream",
-# CHECK-NEXT: plane.jpg ==> "airliner",
+# RUN: FileCheck --input-file %test_temp_dir/squeezenet_1_0_dnnl.txt --check-prefix CHECK-DNNL %s
+# CHECK-DNNL: dog.jpg ==> "Samoyed, Samoyede",
+# CHECK-DNNL: sport.jpg ==> "ski",
+# CHECK-DNNL: food.jpg ==> "ice cream, icecream",
+# CHECK-DNNL: plane.jpg ==> "liner, ocean liner",
