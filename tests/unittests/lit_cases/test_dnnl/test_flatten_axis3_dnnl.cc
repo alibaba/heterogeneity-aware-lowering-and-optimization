1ad29728b3132b81817e6e8a2e741eb6b9831807
shenghui.liu <shliusdu@qq.com>
2021-02-01 20:48:48 +0000

    fix reshape bug and pass unit tests

--- tests/unittests/lit_cases/test_dnnl/test_flatten_axis3_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_flatten_axis3_dnnl.cc
@@ -25,5 +25,5 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_flatten_axis3 | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_flatten_axis3_dnnl.cc.tmp.main.cc.in"
