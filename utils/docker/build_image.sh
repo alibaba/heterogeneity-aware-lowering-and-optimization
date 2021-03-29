9ce25ea49abc8fc4aa61f7c5ed2b37016a111e9e
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2021-03-29 13:39:14 -0700

    [Docker] Update scripts to differentiate runtime/devel images

--- utils/docker/build_image.sh
+++ utils/docker/build_image.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+VER="latest"
+FLAVOR="devel"
+NAMESPACE="registry-intl.us-west-1.aliyuncs.com/computation"
+
 base_image_gpu="nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04"
 base_image_cpu="ubuntu:18.04"
 
 docker build --build-arg BASE_IMAGE=${base_image_cpu} \
-  -t latest-x86_64-ubuntu18.04 .
+  -t $NAMESPACE/halo:$VER-$FLAVOR-x86_64-ubuntu18.04 .
 
 docker build --build-arg BASE_IMAGE=${base_image_gpu} \
-  -t latest-cuda10.0-cudnn7-ubuntu18.04 .
+  -t $NAMESPACE/halo:$VER-$FLAVOR-cuda10.0-cudnn7-ubuntu18.04 .
