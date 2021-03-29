dc9cd8d2525127f6cec8382d5fbcd9091ac8708c
xuhongyao.xhy <xuhongyao.xhy@alibaba-inc.com>
2021-03-29 14:30:18 +0000

    [Images]install Git & some python libs for model conversion

--- utils/docker/build_image.sh
+++ utils/docker/build_image.sh
@@ -4,7 +4,7 @@ base_image_gpu="nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04"
 base_image_cpu="ubuntu:18.04"
 
 docker build --build-arg BASE_IMAGE=${base_image_cpu} \
-  -t halo:0.5-x86_64-ubuntu18.04 .
+  -t latest-x86_64-ubuntu18.04 .
 
 docker build --build-arg BASE_IMAGE=${base_image_gpu} \
-  -t halo:0.5-cuda10.0-cudnn7-ubuntu18.04 .
+  -t latest-cuda10.0-cudnn7-ubuntu18.04 .
