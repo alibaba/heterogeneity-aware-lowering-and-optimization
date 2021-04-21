7056cfa03301164df371a00df02c1a1140e82294
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2021-04-21 18:07:19 +0000

    [CodeGen] Support ops with bool result types

--- tests/unittests/lit_cases/test_tensorrt/test_and_bcast4v4d_tensorrt.cc
+++ tests/unittests/lit_cases/test_tensorrt/test_and_bcast4v4d_tensorrt.cc
@@ -26,5 +26,5 @@
 // RUN: %t_tensorrt.exe 0.0001 0 tensorrt %data_path/test_and_bcast4v4d | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_and_bcast4v4d_tensorrt.cc.tmp.main.cc.in"
