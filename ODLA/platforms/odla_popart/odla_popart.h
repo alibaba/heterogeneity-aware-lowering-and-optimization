3dc44a58e2583074f8ed4410de93a47d558d70e0
youben.yyb <youben.yyb@alibaba-inc.com>
2021-07-21 22:33:12 -0700

    init version of popart pipeline

--- ODLA/platforms/odla_popart/odla_popart.h
+++ ODLA/platforms/odla_popart/odla_popart.h
@@ -28,6 +28,7 @@
 #include <cstddef>
 #include <functional>
 #include <memory>
+#include <mutex>
 #include <numeric>
 #include <popart/builder.hpp>
 #include <popart/dataflow.hpp>
@@ -37,15 +38,87 @@
 #include <popart/session.hpp>
 #include <popart/sessionoptions.hpp>
 #include <popart/stepio.hpp>
+#include <popart/tensordata.hpp>
 #include <popart/tensorinfo.hpp>
 #include <popart/voiddata.hpp>
+#include <queue>
 #include <random>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
+template <typename T>
+class TSQueue {
+ public:
+  TSQueue() = default;
+  TSQueue(const TSQueue<T>&) = delete;
+  TSQueue& operator=(const TSQueue<T>&) = delete;
+  TSQueue(TSQueue<T>&& other) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_ = std::move(other.queue_);
+  }
+  virtual ~TSQueue() {}
+
+  unsigned long size() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return queue_.size();
+  }
+
+  T front() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (queue_.empty()) {
+      return {};
+    }
+    return queue_.front();
+  }
+
+  T back() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (queue_.empty()) {
+      return {};
+    }
+    return queue_.back();
+  }
+
+  void pop() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (queue_.empty()) {
+      return;
+    }
+    queue_.pop();
+    return;
+  }
+
+  void push(const T& data) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.push(data);
+  }
+
+  void push(const T&& data) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.emplace(data);
+  }
+
+  bool empty() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return queue_.empty();
+  }
+
+  void clear() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    while (!queue_.empty()) {
+      queue_.pop();
+    }
+  }
+
+ private:
+  std::queue<T> queue_;
+  mutable std::mutex mutex_;
+};
+
 typedef struct TargetOpts {
   bool use_ipu_model;
+  bool enable_pipeline;
   int64_t ipu_num;
   int64_t batches_per_step;
 } target_opts;
@@ -68,10 +141,16 @@ struct _odla_computation {
   std::unordered_map<std::string, odla_value> outputs_map;
   std::vector<odla_value> input_values;
   std::vector<odla_value> output_values;
+  popart::StepIOCallback::InputCallback input_callback;
+  popart::StepIOCallback::InputCompleteCallback input_complete_callback;
+  popart::StepIOCallback::OutputCallback output_callback;
+  popart::StepIOCallback::OutputCompleteCallback output_complete_callback;
+  TSQueue<popart::ConstVoidData> in_queue;
+  TSQueue<odla_value> out_queue;
   target_opts opts;
 
   _odla_computation(std::unique_ptr<popart::Builder> b)
-      : builder(std::move(b)), opts({false, 1, 1}) {}
+      : builder(std::move(b)), opts({false, false, 1, 1}) {}
 };
 
 struct _odla_context {
