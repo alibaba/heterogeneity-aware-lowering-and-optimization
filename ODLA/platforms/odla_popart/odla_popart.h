74f98b88051f7d2df55734aa1425632d14e81a6c
jackz <jackz@graphcore.ai>
2021-07-25 16:40:21 +0100

    Refactored and add no pipeline multi thread

--- ODLA/platforms/odla_popart/odla_popart.h
+++ ODLA/platforms/odla_popart/odla_popart.h
@@ -65,9 +65,7 @@ struct _odla_value {
 
 struct _odla_computation {
   std::unique_ptr<popart::Builder> builder;
-  // std::map<popart::TensorId, std::unique_ptr<popart::IArray>> inputs;
-  // std::map<popart::TensorId, std::unique_ptr<popart::IArray>> outputs;
-  std::unique_ptr<popart::InferenceSession> session;        //Session变成是图的资源，一个图对应一个设备，一个session，context里面是每次不同的数据
+  std::unique_ptr<popart::InferenceSession> session;
   std::unordered_map<std::string, odla_value> inputs_map;
   std::unordered_map<std::string, odla_value> outputs_map;
   std::vector<odla_value> input_values;
@@ -78,80 +76,42 @@ struct _odla_computation {
       : builder(std::move(b)), opts({false, 1, 1}) {}
 };
 
-#define ODLA_PIPELINE
 struct _odla_context {
   odla_computation comp;
-  //
-  //所有的数据都应该是和session绑定的，computation只定义图相关的，以及执行环境相关的，和数据无关
   std::map<popart::TensorId, std::unique_ptr<popart::IArray>> inputs;
   std::map<popart::TensorId, std::unique_ptr<popart::IArray>> outputs;
-  _odla_context(odla_computation c)
-      : comp(c) {}
-#ifdef ODLA_PIPELINE  
-  std::mutex context_mutex;
-  std::condition_variable context_cv;
-  std::set<popart::TensorId> tensors_visited; //This is the tensor visited by callback
-  std::set<popart::TensorId> tensors_written;  //Record the output tensor written by callback
-  //std::unordered_map<std::string, odla_value> inputs_map;
-  //std::unordered_map<std::string, odla_value> outputs_map;
-  //std::vector<odla_value> input_values;
-  //std::vector<odla_value> output_values;
-
-  // _odla_context(odla_computation c,
-  //               std::unique_ptr<popart::InferenceSession> sess)
-  //     : comp(c), session(std::move(sess)) {}
-  void wait()
-  {
-    std::unique_lock<std::mutex> lock(context_mutex);
-    context_cv.wait(lock);
-  }
-  void notify()
-  {
-    std::unique_lock<std::mutex> lock(context_mutex);
-    context_cv.notify_one();
-  }
-  popart::IArray* get_data_by_tensor_id(popart::TensorId id){
-    auto visited = tensors_visited.find(id);
-    if(tensors_visited.end() != visited){
-      std::cerr << "get_data_by_tensor_id() -> Multiple callback on the same tensor:" << id << std::endl;
-      return NULL;
-    }
-    auto iter = inputs.find(id);
-    if(inputs.end() == iter)
-      return NULL;
-    else
-    {
-      tensors_visited.insert(id);
-      return &(*iter->second);
-    }
-  }
-  popart::IArray* write_data_by_tensor_id(popart::TensorId id){
-    auto written = tensors_written.find(id);
-    if(tensors_written.end() != written){
-      std::cerr << "write_data_by_tensor_id -> Multiple output callback on the same tensor:" << id << std::endl;
-      return NULL;
-    }
-    auto iter = outputs.find(id);
-    if(outputs.end() == iter)
-      return NULL;
-    else
-    {
-      tensors_written.insert(id);
-      return &(*iter->second);
-    }
-  }
-  bool all_tensors_visited(){
-    return (tensors_visited.size() == inputs.size());
-  }
-  bool all_tensors_written(){
-    return (tensors_written.size() == outputs.size());
-  }
-  void clear_visited_and_written(){
-    std::cout << "clear_visited_and_written() -> clear the visited and written record for the context reusing." << std::endl;
-    tensors_visited.clear();
-    tensors_written.clear();
-  }
-#endif  
+  _odla_context(odla_computation c): comp(c) {}
+  virtual void wait() {}
+  virtual void notify() {}
+  virtual popart::IArray* get_data_by_tensor_id(popart::TensorId id) = 0;
+  virtual popart::IArray* write_data_by_tensor_id(popart::TensorId id) = 0;
+  virtual bool all_tensors_visited(){return true;}
+  virtual bool all_tensors_written(){return true;}
+  virtual void clear_visited_and_written(){}
+  virtual bool deletable(){return false;}
 };
 
+struct SingleComp {
+  odla_computation single_comp;
+  std::mutex initalization_mutex;
+  bool comp_initialized = false;
+  static std::mutex instance_mutex;
+  static SingleComp* instance;
+  odla_computation get_comp() { return single_comp; }
+  void init_comp();
+  static SingleComp* get_instance() {
+    if (nullptr == instance) {
+      std::lock_guard<std::mutex> guard(instance_mutex);
+      if (nullptr == instance) {
+        instance = new SingleComp();
+        // Create the single computation
+        std::unique_ptr<popart::Builder> builder = popart::Builder::create();
+        // Place Subgraph on IPU 0
+        builder->setAttribute(popart::sVirtualGraphAttribute, 0);
+        instance->single_comp = new _odla_computation(std::move(builder));
+      }
+    }
+    return instance;
+  }
+};
 #endif
