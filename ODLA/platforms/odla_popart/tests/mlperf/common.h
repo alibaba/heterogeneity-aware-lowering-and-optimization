be0985f553a215488b819cbf46b31111fc9571dc
jackz <jackz@graphcore.ai>
2021-08-13 00:16:28 +0100

    Add common functions to common.h and common.cc

---
+++ ODLA/platforms/odla_popart/tests/mlperf/common.h
@@ -0,0 +1,142 @@
+#include <string>
+#include <map>
+#include <chrono>
+#include "json.hpp"
+#include "cnpy.h"
+
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <ODLA/odla.h>
+#include <thread>
+#include <cstring>
+#include <iomanip>
+
+#include "model.h"
+#include "cnpy.h"
+
+#ifndef __COMMON__H_
+#define __COMMON__H_
+
+struct Float {
+  // TODO(unknown): no infinity, underflow/overflow handling.
+  Float() = delete;
+  static constexpr int BitsPerByte = 8;
+  template <typename T, int exp, int mantissa>
+  static std::array<int, 3> Extract(T x) {
+    static_assert(exp + mantissa + 1 == sizeof(T) * BitsPerByte);
+    int sign = x >> (exp + mantissa);
+    int m = x & ((1 << mantissa) - 1);
+    int e = (x >> mantissa) & ((1 << exp) - 1);
+    return {sign, e, m};
+  }
+
+  template <typename T, int exp, int mantissa>
+  static T Combine(int sign, int e, int m) {
+    static_assert(exp + mantissa + 1 == sizeof(T) * BitsPerByte);
+    T x{0};
+    x = sign ? 1U << (exp + mantissa) : 0;
+    m >>= 32 - mantissa;
+    x |= m & ((1U << mantissa) - 1);
+    x |= (e & ((1U << exp) - 1)) << mantissa;
+    return x;
+  }
+  static constexpr int FP32Exp = 8;
+  static constexpr int FP32Mantissa = 23;
+  static constexpr int FP32ExpBias = 127;
+  static constexpr int FP16Exp = 5;
+  static constexpr int FP16Mantissa = 10;
+  static constexpr int FP16ExpBias = 15;
+
+  static float GetFP32(uint16_t x) {
+    auto components = Extract<uint16_t, FP16Exp, FP16Mantissa>(x);
+    components[1] -= FP16ExpBias;
+    components[2] <<= 32 - FP16Mantissa;
+    // Underflow.
+    if (components[1] == -FP16ExpBias) {
+      while (components[2] > 0) {
+        --components[1];
+        components[2] <<= 1;
+      }
+      components[2] <<= 1;
+    }
+    return GetFP32(components[0], components[1], components[2]);
+  }
+
+  static float GetFP32(uint8_t sign, int32_t e, uint32_t m) {
+    uint32_t x =
+        Combine<uint32_t, FP32Exp, FP32Mantissa>(sign, e + FP32ExpBias, m);
+    return *(reinterpret_cast<float*>(&x)); // NOLINT.
+  }
+};
+
+/***************************************************************
+ * The size of the parameters was defined by the npz file
+ *   only read the npz file to get the data. So we don't need 
+ *   the *batch_size* & *batches_per_step* information here.
+ * 
+ * Parameters needed for the run function:
+ * {
+ *    "model":"model_path_with_name",
+ *    "thread_number":1,
+ *    "duration":"40m",
+ *    "npz":"npz_file_path_with_name",
+ *    "inputs":
+ *    {
+ *      0:["indices", "UNIT32"], 
+ *      1:["masks", "UNIT32"], 
+ *      2:["positions", "UINT32"], 
+ *      3:["segments", "UINT32"]
+ *    },
+ *    "outputs":
+ *    {
+ *        0:["squad_gemm", "FP16"]
+ *        1:["someother", "FP16"]
+ *    }
+ * }
+***************************************************************/
+class Config{
+private:
+    std::string _model_path;
+    std::uint32_t _thread_number;
+    std::uint32_t _duration;
+    std::string _npz_file_path;
+    std::map<std::string, std::vector<std::string>> _inputs;
+    std::map<std::string, std::vector<std::string>> _outputs;
+    std::string _result_file;
+    std::uint32_t _thread_buffer_cnt;
+    static Config* _instance;
+    Config(){};
+public:
+    ~Config(){};
+    static Config* instance(){return _instance;}
+    const std::string& model_path(){return _model_path;}
+    const std::uint32_t thread_number(){return _thread_number;}
+    const std::uint32_t thread_buffer_cnt(){return _thread_buffer_cnt;}
+    const std::uint32_t duration(){return _duration;}
+    const std::string& npz_file_path(){return _npz_file_path;}
+    const std::string& result_file(){return _result_file;}
+    const std::map<std::string, std::vector<std::string>>& inputs() {return _inputs;}
+    const std::map<std::string, std::vector<std::string>>& outputs() {return _outputs;}
+    void load(const std::string& file_path);
+    void print();
+};
+
+class BaseTest{
+public:
+    BaseTest(){};
+    ~BaseTest(){};
+    void start();
+    virtual void do_inference(cnpy::npz_t& data) = 0;
+    void save_latency_results(const std::vector<float>& latencies, 
+        const std::map<std::string, std::vector<cnpy::NpyArray>>& results); 
+private:
+    cnpy::npz_t* prepare_data();
+};
+
+extern void inference(int thread_id, cnpy::npz_t* all_data, BaseTest* test);
+
+#endif//__COMMON__H_
--- ODLA/platforms/odla_popart/tests/mlperf/main.cc
+++ ODLA/platforms/odla_popart/tests/mlperf/main.cc
@@ -6,127 +6,32 @@
 #include <ctime>
 #include <ODLA/odla.h>
 #include <thread>
+#include <cstring>
+#include <iomanip>
 
 #include "model.h"
 #include "cnpy.h"
+#include "common.h"
 
 using namespace std;
 
-#define TOTAL_DATA_NUM 128//0
-
-struct Float {
-  // TODO(unknown): no infinity, underflow/overflow handling.
-  Float() = delete;
-  static constexpr int BitsPerByte = 8;
-  template <typename T, int exp, int mantissa>
-  static std::array<int, 3> Extract(T x) {
-    static_assert(exp + mantissa + 1 == sizeof(T) * BitsPerByte);
-    int sign = x >> (exp + mantissa);
-    int m = x & ((1 << mantissa) - 1);
-    int e = (x >> mantissa) & ((1 << exp) - 1);
-    return {sign, e, m};
-  }
-
-  template <typename T, int exp, int mantissa>
-  static T Combine(int sign, int e, int m) {
-    static_assert(exp + mantissa + 1 == sizeof(T) * BitsPerByte);
-    T x{0};
-    x = sign ? 1U << (exp + mantissa) : 0;
-    m >>= 32 - mantissa;
-    x |= m & ((1U << mantissa) - 1);
-    x |= (e & ((1U << exp) - 1)) << mantissa;
-    return x;
-  }
-  static constexpr int FP32Exp = 8;
-  static constexpr int FP32Mantissa = 23;
-  static constexpr int FP32ExpBias = 127;
-  static constexpr int FP16Exp = 5;
-  static constexpr int FP16Mantissa = 10;
-  static constexpr int FP16ExpBias = 15;
-
-  static float GetFP32(uint16_t x) {
-    auto components = Extract<uint16_t, FP16Exp, FP16Mantissa>(x);
-    components[1] -= FP16ExpBias;
-    components[2] <<= 32 - FP16Mantissa;
-    // Underflow.
-    if (components[1] == -FP16ExpBias) {
-      while (components[2] > 0) {
-        --components[1];
-        components[2] <<= 1;
-      }
-      components[2] <<= 1;
-    }
-    return GetFP32(components[0], components[1], components[2]);
-  }
-
-  static float GetFP32(uint8_t sign, int32_t e, uint32_t m) {
-    uint32_t x =
-        Combine<uint32_t, FP32Exp, FP32Mantissa>(sign, e + FP32ExpBias, m);
-    return *(reinterpret_cast<float*>(&x)); // NOLINT.
+//static cnpy::npz_t *all_data = nullptr;
+class MLPerfTest : public BaseTest{
+public:
+  MLPerfTest(){};
+  ~MLPerfTest(){};
+  void do_inference(cnpy::npz_t& data) override
+  {
+    model(data["indices"].data<std::uint32_t>(), 
+          data["masks"].data<std::uint32_t>(),
+          data["positions"].data<std::uint32_t>(),
+          data["segments"].data<std::uint32_t>(), 
+          data["squad_gemm"].data<odla_float16>()); 
   }
 };
 
-static cnpy::npz_t *all_data = nullptr;
-
-void prepare_data()
-{
-  all_data = new cnpy::npz_t[TOTAL_DATA_NUM];
-  cnpy::npz_t one_data = cnpy::npz_load("../data/bs10_bps128.npz"); 
-  for(int i=0; i < TOTAL_DATA_NUM; i++)
-    all_data[i] = one_data;
-}
-
-void inference(int start, int count, cnpy::npz_t* all_data){
-  for(int i=start; i < start + count; i++){
-    cnpy::npz_t* data = (all_data)+i;
-    auto start_t = std::chrono::steady_clock::now();
-	  model((*data)["indices"].data<std::uint32_t>(), 
-            (*data)["masks"].data<std::uint32_t>(),
-            (*data)["positions"].data<std::uint32_t>(),
-            (*data)["segments"].data<std::uint32_t>(), 
-            (*data)["squad_gemm"].data<std::uint16_t>()); 
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start_t;
-    std::cout << "[" << i << "] @@@@@ request latency is: " << elapsed_seconds.count() << "s" << std::endl;
-  }
-}
-
-#define THREAD_COUNT 32
-
 int main(){
-
-  prepare_data();
-  bool single_thread = true;
-
-  auto start = std::chrono::steady_clock::now();
-  if(single_thread){
-    for (int step=0; step<TOTAL_DATA_NUM; step++){
-      cnpy::npz_t* data = all_data+step;
-	    if(100 == step)
-		    start = std::chrono::steady_clock::now();
-
-      model((*data)["indices"].data<std::uint32_t>(), 
-            (*data)["masks"].data<std::uint32_t>(),
-            (*data)["positions"].data<std::uint32_t>(),
-            (*data)["segments"].data<std::uint32_t>(), 
-            (*data)["squad_gemm"].data<std::uint16_t>()); 
-	  }
-  }
-  else{
-    std::thread threads[THREAD_COUNT];
-    int start_index = 0;
-    int count = TOTAL_DATA_NUM/THREAD_COUNT;
-    for(int i=0; i < THREAD_COUNT; i++){
-      threads[i] = std::thread(inference, start_index, count, all_data);
-      start_index += count;
-    }
-    cout << "Threads started, wait for all threads end." << std::endl;
-    for(int i=0; i < THREAD_COUNT; i++){
-      threads[i].join();
-    }
-  }
-  auto end = std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end-start;
-  std::cout << "The total run time including compile is: " << elapsed_seconds.count() << "s" << std::endl;
+  MLPerfTest test;
+  test.start();
   model_fini();
 }
