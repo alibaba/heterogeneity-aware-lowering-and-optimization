31a6b3ede23994f191492220e00b34a7ed47032d
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2021-08-18 05:47:49 +0000

    [ODLA/DNNL] Add support for reduction_l2

--- tests/unittests/lit_cases/test_dnnl/test_reduce_l1_keep_dims_example_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_reduce_l1_keep_dims_example_dnnl.cc
@@ -25,5 +25,5 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_reduce_l1_keep_dims_example | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_reduce_l1_keep_dims_example_dnnl.cc.tmp.main.cc.in"
