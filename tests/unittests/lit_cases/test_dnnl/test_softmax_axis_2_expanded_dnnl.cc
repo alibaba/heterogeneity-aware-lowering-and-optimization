491d671710a09713380bed12f4baefb703c913de
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2021-08-18 21:02:47 +0000

    [ONNX] Add support for reduce_sum

--- tests/unittests/lit_cases/test_dnnl/test_softmax_axis_2_expanded_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_softmax_axis_2_expanded_dnnl.cc
@@ -25,5 +25,5 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_softmax_axis_2_expanded | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_softmax_axis_2_expanded_dnnl.cc.tmp.main.cc.in"
