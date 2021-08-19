1d0cde6964a4c4f3ab63585f8826fd4c01099408
yanwei <yw01041751@alibaba-inc.com>
2021-08-19 09:09:04 +0800

    update popart api to sdk 2.2

--- ODLA/platforms/odla_popart/custom_ops/rsqrt.cc
+++ ODLA/platforms/odla_popart/custom_ops/rsqrt.cc
@@ -65,8 +65,10 @@ RsqrtOpx::RsqrtOpx(popart::Op * op, popart::popx::Devicex * devicex)
 }
 
 void RsqrtOpx::grow(poplar::program::Sequence & prog) const {
-  setOutTensor(0, popops::map(graph().getPoplarGraph(), popops::expr::UnaryOpType::RSQRT,
-                              getInTensor(0), prog, debugContext()));
+  auto result = popops::map(graph().getPoplarGraph(),
+		                    popops::expr::UnaryOpType::RSQRT,
+			                getInTensor(0).getPoplarTensor(), prog, debugContext());
+  setOutTensor(0, snap::Tensor{result, graph()});
 }
 
 namespace {
