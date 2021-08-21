1c9707f5e8bc35d38fdb917a4be19e1d5b753aff
gcuser <gcuser@alibaba-inc.com>
2021-08-21 11:14:21 +0000

    make 2.1 failed test-case pass in poplar sdk 2.2

--- tests/unittests/lit_cases/test_popart/test_constant_popart.cc
+++ tests/unittests/lit_cases/test_popart/test_constant_popart.cc
@@ -24,5 +24,4 @@
 // RUN: %t_popart.exe 0.0001 0 popart %data_path/test_constant | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
 #include "test_constant_popart.cc.tmp.main.cc.in"
