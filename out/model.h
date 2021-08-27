720c2812fb7bdeaa3f8afcea1e9aed6e5de83aa9
gcuser <jackz@graphcore.ai>
2021-08-27 11:04:18 +0000

    Remove the function test codes of the odla_popart

--- out/model.h
+++
@@ -1,9 +0,0 @@
-//===- Halo Compiler Generated File --------------------------------===//
-
-typedef struct _odla_computation *odla_computation;
-extern "C" {
-void mnist_simple(const float x[1 * 784], float out_y[1 * 10]);
-void mnist_simple_init();
-void mnist_simple_fini();
-odla_computation mnist_simple_helper();
-};
