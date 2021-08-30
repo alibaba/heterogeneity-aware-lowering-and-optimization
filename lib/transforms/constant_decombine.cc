d9b9891dd10a8404226b3eaf0cd905e97bf08f27
youben.yyb <youben.yyb@alibaba-inc.com>
2021-08-29 23:08:01 -0700

    code review

--- lib/transforms/constant_decombine.cc
+++ lib/transforms/constant_decombine.cc
@@ -1,6 +1,6 @@
 //===- constant_decombine.cc ----------------------------------------------===//
 //
-// Copyright (C) 2019-2020 Alibaba Group Holding Limited.
+// Copyright (C) 2019-2021 Alibaba Group Holding Limited.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ namespace halo {
 // constant
 bool ConstantDecombine::RunOnFunction(Function* func) {
   bool changed = false;
-  // Duplicate constants.
   ConstantBuilder cb(func);
   Function::ConstantList& constants = func->Constants();
   for (auto it = constants.begin(), ie = constants.end(); it != ie; ++it) {
@@ -41,7 +40,6 @@ bool ConstantDecombine::RunOnFunction(Function* func) {
       VLOG(1) << "constant node name: " << (*it)->GetName();
       for (auto& u : uses) {
         VLOG(1) << "use operand idx: " << u.GetUseOperandIdx();
-        // u.ReplaceAllUsesWith(u.GetUseOperandIdx(), Def{constant_copy[i], 0});
         constant_copy[i] = cb.Clone(*(*it));
         constant_copy[i]->SetName((*it)->GetName() + "_copy_" +
                                   std::to_string(i));
