5d47cb7311de5d543ef59108bd086a569580d395
tianboh <haotianbo.htb@alibaba-inc.com>
2021-09-07 06:30:40 +0000

    Add support for dnnl tan

--- ODLA/platforms/dnnl/odla_dnnl.cc
+++ ODLA/platforms/dnnl/odla_dnnl.cc
@@ -17,10 +17,13 @@
 
 #include "odla_dnnl.h"
 
+#include <stdio.h>
+
 #include <cassert>
 #include <cmath>
 #include <cstddef>
 #include <cstring>
+#include <eigen-3.3.7/Eigen/Core>
 #include <iostream>
 #include <numeric>
 #include <unordered_map>
@@ -808,6 +811,43 @@ odla_value odla_Shift(odla_value input, odla_value shift_amount,
   };
   add_op(op);
 
+  InterpretIfNeeded();
+  return v;
+}
+
+template <typename T>
+static void tan(void* dst, const void* input, int n) {
+  const T* input_t = static_cast<const T*>(input);
+  T* dst_t = static_cast<T*>(dst);
+  Eigen::Map<const Eigen::Array<T, Eigen::Dynamic, 1>> in(input_t, n);
+  Eigen::Map<Eigen::Array<T, Eigen::Dynamic, 1>> out(dst_t, n);
+  out = in.tan();
+}
+
+odla_value odla_Tan(odla_value input, const odla_value_id value_id) {
+  // Extract type and size
+  auto elem_type = input->elem_type;
+  int n = GetTotalElements(input->shape);
+  // Prepare destination memory
+  dnnl::memory dst_mem;
+  dnnl::memory::desc dst_md = getMemoryDesc({elem_type, input->shape});
+  dst_mem = dnnl::memory(dst_md, g_comp->eng);
+  auto v = CreateValue(dst_mem, input->shape, value_id);
+  v->elem_type = elem_type;
+  // Create lambda operation
+  auto op = [input, dst_mem, n] {
+    void* dst = dst_mem.get_data_handle();
+    const void* data = input->mem.get_data_handle();
+    if (input->elem_type == ODLA_FLOAT32) {
+      tan<float>(dst, data, n);
+    } else if (input->elem_type == ODLA_FLOAT64) {
+      tan<double>(dst, data, n);
+    } else {
+      assert(0);
+    }
+  };
+  // Postprocess
+  add_op(op);
   InterpretIfNeeded();
   return v;
 }
