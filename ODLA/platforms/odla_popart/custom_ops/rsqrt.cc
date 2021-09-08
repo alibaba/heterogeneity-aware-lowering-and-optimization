f18b61a39fd0249684fe970e3d7fc688ade72d2c
gcuser <jackz@graphcore.ai>
2021-09-08 05:12:50 +0000

    Change back to SDK 2.1

--- ODLA/platforms/odla_popart/custom_ops/rsqrt.cc
+++ ODLA/platforms/odla_popart/custom_ops/rsqrt.cc
@@ -70,7 +70,7 @@ RsqrtOpx::RsqrtOpx(popart::Op* op, popart::popx::Devicex* devicex)
 void RsqrtOpx::grow(poplar::program::Sequence& prog) const {
   setOutTensor(
       0, popops::map(graph().getPoplarGraph(), popops::expr::UnaryOpType::RSQRT,
-                     getInTensor(0).getPoplarTensor(), prog, debugContext()));
+                     getInTensor(0), prog, debugContext()));
 }
 
 namespace {
