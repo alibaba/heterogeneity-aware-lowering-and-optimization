bdf9cdbac57dd15aa43b62cd216db73576193db1
gcuser <jackz@graphcore.ai>
2021-09-09 02:23:22 +0000

    fix on default configuration & computation destroyment

--- ODLA/platforms/odla_popart/odla_compute.cc
+++ ODLA/platforms/odla_popart/odla_compute.cc
@@ -112,11 +112,7 @@ odla_status odla_DestroyContext(odla_context ctx) {
 
 odla_status odla_DestroyComputation(odla_computation comp) {
   comp->mark_done();
-  if (comp->session != nullptr) {
-    comp->session->getDevice().getDeviceInfo()->detach();
-    comp->session.reset();
-    assert(comp->session == nullptr);
-  }
+  _odla_computation::destruct();
   return ODLA_SUCCESS;
 }
 
@@ -178,7 +174,7 @@ odla_status odla_BindToArgument(odla_value value, const odla_void* data_ptr,
   // only the SEQUENCE model need to pass the data in once time
   if (PopartConfig::instance()->execution_mode() == SEQUENCE &&
       shape.size() > 0)
-    shape[0] *= PopartConfig::instance()->batches_per_step();
+    shape[0] *= g_comp->opts.batches_per_step;
   std::unique_ptr<popart::IArray> p_array = MakeNDArrayWrapper(
       data_ptr, context->comp->builder->getTensorDataType(value->tensor_id),
       shape);
@@ -236,7 +232,7 @@ odla_status odla_BindToOutput(odla_value value, odla_void* data_ptr,
   // only the SEQUENCE model need to pass the data in once time
   if (PopartConfig::instance()->execution_mode() == SEQUENCE &&
       shape.size() > 0)
-    shape[0] *= PopartConfig::instance()->batches_per_step();
+    shape[0] *= g_comp->opts.batches_per_step;
   std::unique_ptr<popart::IArray> p_array = MakeNDArrayWrapper(
       data_ptr, context->comp->builder->getTensorDataType(value->tensor_id),
       shape);
