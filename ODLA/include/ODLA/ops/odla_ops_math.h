1cb6c75225fc3a4d75a0ac7d607c8e0a12d6a47f
haotianbo.htb <haotianbo.htb@alibaba-inc.com>
2021-09-10 08:05:44 +0000

    [ONNX] Add support for cos, sin, acos, asin, atan, cosh, sinh, ceil, isnan

--- ODLA/include/ODLA/ops/odla_ops_math.h
+++ ODLA/include/ODLA/ops/odla_ops_math.h
@@ -309,6 +309,31 @@ extern ODLA_API_EXPORT odla_value ODLA_API_CALL odla_GreaterOrEqual(
 extern ODLA_API_EXPORT odla_value ODLA_API_CALL
 odla_Inverse(odla_value input, const odla_value_id value_id);
 
+//! \brief Check infinity
+/*!
+  IsNaN returns the bool array \p input.
+
+  \param input the input value
+  \param value_id a unique value id (can be NULL)
+
+  \return odla_bool
+*/
+extern ODLA_API_EXPORT odla_value ODLA_API_CALL
+odla_IsInf(odla_value input, odla_bool detect_negative,
+           odla_bool detect_positive, const odla_value_id value_id);
+
+//! \brief Check whether element of input is a number
+/*!
+  IsNaN returns the bool array \p input.
+
+  \param input the input value
+  \param value_id a unique value id (can be NULL)
+
+  \return odla_bool
+*/
+extern ODLA_API_EXPORT odla_value ODLA_API_CALL
+odla_IsNaN(odla_value input, const odla_value_id value_id);
+
 //! \brief "Less Than" test
 /*!
   Less tests if \p lhs is less than \p rhs.
@@ -734,6 +759,19 @@ odla_Sqrt(odla_value input, const odla_value_id value_id);
 */
 extern ODLA_API_EXPORT odla_value ODLA_API_CALL
 odla_Sub(odla_value lhs, odla_value rhs, const odla_value_id value_id);
+
+//! \brief Cos
+/*!
+  Computes sine of \p x element-wise.
+
+  \param x input value
+  \param id the value id assigned to the result
+  \param value_id a unique value id (can be NULL)
+
+  \return odla_value
+*/
+extern ODLA_API_EXPORT odla_value ODLA_API_CALL
+odla_Cos(odla_value x, const odla_value_id value_id);
 
 //! \brief Sin
 /*!
