08487403c75c448cebd370fc1eee77162a921119
maruiyan.mry <maruiyan.mry@alibaba-inc.com>
2021-09-13 23:03:45 -0700

    fix:remove xfail

--- tests/unittests/lit_cases/test_tensorrt/test_thresholdedrelu_example_tensorrt.cc
+++ tests/unittests/lit_cases/test_tensorrt/test_thresholdedrelu_example_tensorrt.cc
@@ -25,5 +25,4 @@
 // RUN: %t_tensorrt.exe 0.0001 0 tensorrt %data_path/test_thresholdedrelu_example | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
 #include "test_thresholdedrelu_example_tensorrt.cc.tmp.main.cc.in"
