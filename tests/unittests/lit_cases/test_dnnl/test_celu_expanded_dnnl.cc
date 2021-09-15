38cfebb77d0f9bbd64ee16462008170d13fd3b3d
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2021-09-15 19:43:41 +0000

    [ODLA/DNNL]: Fix broadcasting of operands

--- tests/unittests/lit_cases/test_dnnl/test_celu_expanded_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_celu_expanded_dnnl.cc
@@ -25,5 +25,5 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_celu_expanded | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_celu_expanded_dnnl.cc.tmp.main.cc.in"
