760b5c76beeca1ab3da5a7927ff1a80c8767f1f5
maruiyan.mry <maruiyan.mry@alibaba-inc.com>
2021-09-28 23:38:29 -0700

    fix:fix support for ONNX gemm op

--- tests/unittests/lit_cases/test_dnnl/test_gemm_all_attributes_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_gemm_all_attributes_dnnl.cc
@@ -27,5 +27,4 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_gemm_all_attributes | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
 #include "test_gemm_all_attributes_dnnl.cc.tmp.main.cc.in"
--- tests/unittests/lit_cases/test_dnnl/test_gemm_alpha_dnnl.cc
+++ tests/unittests/lit_cases/test_dnnl/test_gemm_alpha_dnnl.cc
@@ -27,5 +27,4 @@
 // RUN: %t_dnnl.exe 0.0001 0 dnnl %data_path/test_gemm_alpha | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
 #include "test_gemm_alpha_dnnl.cc.tmp.main.cc.in"
