1f424d31500735ab52342731ee8e14ae05ce91a0
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2021-09-29 14:53:59 -0700

    [ODLLA/TRT] switch to TRT 8.0.3 w/ CUDA 10.2

--- utils/docker/build_image.sh
+++ utils/docker/build_image.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-VER="0.7.2"
+VER="0.7.3"
 FLAVOR="devel"
 NAMESPACE="registry-intl.us-west-1.aliyuncs.com/computation"
 
-base_image_gpu="nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04"
+base_image_gpu="nvidia/cuda:10.2-cudnn8-devel-ubuntu18.04"
 base_image_cpu="ubuntu:18.04"
 
-docker build --build-arg BASE_IMAGE=${base_image_cpu} \
-  -t $NAMESPACE/halo:$VER-$FLAVOR-x86_64-ubuntu18.04 .
+#docker build --build-arg BASE_IMAGE=${base_image_cpu} \
+#  -t $NAMESPACE/halo:$VER-$FLAVOR-x86_64-ubuntu18.04 .
 
 docker build --build-arg BASE_IMAGE=${base_image_gpu} \
-  -t $NAMESPACE/halo:$VER-$FLAVOR-cuda10.0-cudnn7-ubuntu18.04 .
+  -t $NAMESPACE/halo:$VER-$FLAVOR-cuda10.2-cudnn8-ubuntu18.04 .
 
