c2afe25e149fe01a0e7a5551d74389204a5af333
weimingzha0 <weimingzha0@users.noreply.github.com>
2021-12-09 22:42:11 +0000

    deploy: 1546fde0a005fa9ab73cdfc4cd01078931e5a617

--- odla_docs/html/search/enums_0.js
+++ odla_docs/html/search/enums_0.js
@@ -7,7 +7,7 @@ var searchData=
   ['odla_5ffill_5fmethod_586',['odla_fill_method',['../odla__ops__process_8h.html#ae5784f32274737503651496765cc159c',1,'odla_ops_process.h']]],
   ['odla_5finterpolation_5fmode_587',['odla_interpolation_mode',['../odla__ops__process_8h.html#a527558ca73adc6469aadb7171cb0dfac',1,'odla_ops_process.h']]],
   ['odla_5fitem_5ftype_588',['odla_item_type',['../odla__compute_8h.html#a6be789aa7177278446e347afcfb9aa0b',1,'odla_compute.h']]],
-  ['odla_5floop_5foutput_5fmode_589',['odla_loop_output_mode',['../odla__ops__loop_8h.html#a3fa6f4b753812a9e096934d08678b896',1,'odla_ops_loop.h']]],
+  ['odla_5floop_5foutput_5fmode_589',['odla_loop_output_mode',['../odla__ops__control__flow_8h.html#a3fa6f4b753812a9e096934d08678b896',1,'odla_ops_control_flow.h']]],
   ['odla_5fmemory_5flayout_590',['odla_memory_layout',['../odla__ops__nn_8h.html#a54eaa7020ac01fc2a354d4fef9313a62',1,'odla_ops_nn.h']]],
   ['odla_5fresize_5fcoordinate_5fmode_591',['odla_resize_coordinate_mode',['../odla__ops__process_8h.html#afe771e362734750b05028ed08414761d',1,'odla_ops_process.h']]],
   ['odla_5frnn_5fdirection_592',['odla_rnn_direction',['../odla__ops__nn_8h.html#a9270ccb090ddf6229c74cd04026120a0',1,'odla_ops_nn.h']]],
