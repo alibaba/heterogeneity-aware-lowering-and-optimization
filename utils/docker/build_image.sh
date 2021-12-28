f53b25a2c6b3c1581cd61099272deeeb4637ea52
zh-wei <wujiang.wz@alibaba-inc.com>
2021-12-28 08:08:30 +0000

    [Docker] Change cuda verion to 10.2 for cmb branch.

--- utils/docker/build_image.sh
+++ utils/docker/build_image.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -xe
 
-VER="0.7.4"
+VER="0.7.3"
 FLAVOR="devel"
 NAMESPACE="registry-intl.us-west-1.aliyuncs.com/computation"
 
-base_image_gpu="nvidia/cuda:11.3.1-cudnn8-devel-ubuntu18.04"
+base_image_gpu="nvidia/cuda:10.2-cudnn8-devel-ubuntu18.04"
 base_image_cpu="ubuntu:18.04"
 
 #docker build --build-arg BASE_IMAGE=${base_image_cpu} \
 #  -t $NAMESPACE/halo:$VER-$FLAVOR-x86_64-ubuntu18.04 .
 
 docker build --build-arg BASE_IMAGE=${base_image_gpu} \
-  -t $NAMESPACE/halo:$VER-$FLAVOR-cuda11.3.1-cudnn8-ubuntu18.04 .
+  -t $NAMESPACE/halo:$VER-$FLAVOR-cuda10.2-cudnn8-ubuntu18.04 .
 
