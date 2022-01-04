1761af0960d20016d9ecb0c57b97a8ac7d564dc9
zh-wei <zh-wei@users.noreply.github.com>
2022-01-04 11:15:34 +0000

    deploy: 850c4ab5ade75f60951ddad3a8816d2073501dba

--- odla_docs/html/search/classes_0.js
+++ odla_docs/html/search/classes_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['odla_5ftask_330',['odla_task',['../structodla__task.html',1,'']]],
-  ['odla_5fvalue_5fids_331',['odla_value_ids',['../structodla__value__ids.html',1,'']]],
-  ['odla_5fvalue_5fquant_5finfo_332',['odla_value_quant_info',['../structodla__value__quant__info.html',1,'']]],
-  ['odla_5fvalue_5fshape_333',['odla_value_shape',['../structodla__value__shape.html',1,'']]],
-  ['odla_5fvalue_5ftype_334',['odla_value_type',['../structodla__value__type.html',1,'']]],
-  ['odla_5fvalues_335',['odla_values',['../structodla__values.html',1,'']]]
+  ['odla_5ftask_324',['odla_task',['../structodla__task.html',1,'']]],
+  ['odla_5fvalue_5fids_325',['odla_value_ids',['../structodla__value__ids.html',1,'']]],
+  ['odla_5fvalue_5fquant_5finfo_326',['odla_value_quant_info',['../structodla__value__quant__info.html',1,'']]],
+  ['odla_5fvalue_5fshape_327',['odla_value_shape',['../structodla__value__shape.html',1,'']]],
+  ['odla_5fvalue_5ftype_328',['odla_value_type',['../structodla__value__type.html',1,'']]],
+  ['odla_5fvalues_329',['odla_values',['../structodla__values.html',1,'']]]
 ];
