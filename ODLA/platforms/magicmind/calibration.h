d4d916edbabea362000fc4a959d1bc00c01fd797
tjs2200120 <tjs@test.com>
2022-02-10 11:36:27 +0000

    add odla_magicmind

---
+++ ODLA/platforms/magicmind/calibration.h
@@ -0,0 +1,81 @@
+#ifndef CALIBRATION_H
+#define CALIBRATION_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "interface_calibrator.h"
+
+#define LOGINFO(fmt, args...) fprintf(stdout, "[MMINFO]  " fmt "\n", ##args)
+
+template <typename T>
+void getDataFromFile(const std::string path,
+                     std::vector<std::string> files,
+                     T *data_ptr,
+                     int datasize) {
+  char *temp_data = (char *)data_ptr;
+  for (auto file : files) {
+    std::string temp_path = path + file;
+    std::ifstream inFile(temp_path, std::ios::in | std::ios::binary);
+    if (!inFile) {
+      LOGINFO("Open file %s failed.", temp_path.c_str());
+      continue;
+    }
+    inFile.read(temp_data, datasize);
+    inFile.close();
+    temp_data += datasize;
+  }
+}
+
+class FixedCalibData : public magicmind::CalibDataInterface {
+ public:
+  FixedCalibData(const magicmind::Dims &shape,
+                 const magicmind::DataType &data_type,
+                 int max_samples,
+                 std::vector<std::string> &data_paths){
+    shape_ = shape;
+    data_type_ = data_type;
+    batch_size_ = shape.GetDimValue(0);
+    max_samples_ = max_samples;
+    data_paths_ = data_paths;
+    current_sample_ = 0;
+    buffer_.resize(shape.GetElementCount());
+  }
+
+  magicmind::Dims GetShape() const { return shape_;}
+  magicmind::DataType GetDataType() const {return data_type_;}
+  void *GetSample() { return buffer_.data();}
+
+  magicmind::Status Next() {
+    if (current_sample_ + batch_size_ > max_samples_) {
+      std::string msg = "sample number is bigger than max sample number!\n";
+      magicmind::Status status_(magicmind::error::Code::INTERNAL, msg);
+      return status_;
+    }
+
+    auto data_size = sizeof(float) * shape_.GetElementCount();
+    std::vector<std::string> temp_paths(data_paths_.begin() + current_sample_,
+                                        data_paths_.begin() + current_sample_ + batch_size_);
+    getDataFromFile("",temp_paths,  buffer_.data(),data_size);
+
+    current_sample_ += batch_size_;
+    return magicmind::Status::OK();
+  }
+
+  magicmind::Status Reset() {
+    current_sample_ = 0;
+    return magicmind::Status::OK();
+  }
+
+ private:
+  magicmind::Dims shape_;
+  magicmind::DataType data_type_;
+  int batch_size_;
+  int max_samples_;
+  int current_sample_;
+  std::vector<std::string> data_paths_;
+  std::vector<float> buffer_;
+};
+
+#endif
---
+++ ODLA/platforms/magicmind/utils.h
@@ -0,0 +1,18 @@
+#ifndef MAGICMIND_UTILS_H_
+#define MAGICMIND_UTILS_H_
+
+class EnvTime {
+ public:
+  static constexpr uint64_t kSecondsToNanos = 1000ULL * 1000ULL * 1000ULL;
+
+  EnvTime()          = default;
+  virtual ~EnvTime() = default;
+
+  static uint64_t NowNanos() {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return (static_cast<uint64_t>(ts.tv_sec) * kSecondsToNanos + static_cast<uint64_t>(ts.tv_nsec));
+  }
+};
+
+#endif
