d95d98138101e13c0b9604f082a4f14e2107bc46
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2022-02-16 11:02:38 -0800

    [ODLA] Add async call back support

--- ODLA/include/ODLA/odla_compute.h
+++ ODLA/include/ODLA/odla_compute.h
@@ -53,6 +53,8 @@ typedef enum {
   ODLA_LOAD_ENGINE_MODE,
   ODLA_ENABLE_ENGINE_CACHE,
   ODLA_CACHE_DIR,
+  ODLA_ASYNC_CALLBACK_FUNC,
+  ODLA_ASYNC_CALLBACK_ARG,
 } odla_item_type;
 
 //! \brief BF16 mode
