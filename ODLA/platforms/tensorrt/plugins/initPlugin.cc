b8cd468eb17d91641edc8a412c18ad3c89cc953f
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2022-04-13 16:49:27 -0700

    Fix ODLA TRT plugin build
    Some common files (e.g. checkMacrosPlugin.cpp) were included in
    odla plugin, which causes errors using logging because both libs have
    the same logger definition.
    
    (cherry picked from commit 0d03954f87032991c6e54941ac1747aaad279b97)

--- ODLA/platforms/tensorrt/plugins/initPlugin.cc
+++ ODLA/platforms/tensorrt/plugins/initPlugin.cc
@@ -31,7 +31,7 @@ void initODLAPlugin(nvinfer1::ILogger* logger, const char* libNamespace) {
 #else
   const int NUM_PLUGINS = 32;
 #endif
-  initLibNvInferPlugins(static_cast<void*>(logger), "");
+  initLibNvInferPlugins(static_cast<void*>(logger), libNamespace);
   REGISTER_TENSORRT_PLUGIN(BatchedNMSPluginV2Creator);
   REGISTER_TENSORRT_PLUGIN(OneHotPluginCreator);
 
