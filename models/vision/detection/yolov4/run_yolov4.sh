aeb2eb17b2b7580feb78c83f040d684768d09ec8
wangcl15 <1373555614@qq.com>
2022-04-28 15:17:06 +0800

    [Models] add halo test demo for yolov4 and yolov5

---
+++ models/vision/detection/yolov4/run_yolov4.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+set -ex
+
+model="yolov4"
+cntPath=$(cd `dirname $0`;pwd)
+
+modelPath=$cntPath/model/$model.onnx
+outputDir=$cntPath/out
+
+setEnvVar() {
+	export INSTALL_DIR=/opt/halo
+	export HALO_BIN=$INSTALL_DIR/bin/halo
+	export ODLA_INC=$INSTALL_DIR/include
+	export ODLA_LIB=$INSTALL_DIR/lib
+	export ODLA_TRT_USE_EXPLICIT_BATCH=1
+	export YOLOV_OUTPUT_PATH=$outputDir
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ODLA_LIB
+}
+
+checkOutput() {
+	outputPath=$outputDir
+	if [ -d $outputPath ];then
+		rm -rf $outputPath
+	fi
+	mkdir -p $outputPath
+}
+
+compileModelWithHalo() {
+	# compile models with HALO
+	$HALO_BIN  $modelPath \
+		-target cxx \
+		-disable-broadcasting \
+		-entry-func-name=model \
+		-o $outputPath/$model.cc \
+		--inputs=input_1:0
+		
+	g++ $outputPath/$model.cc \
+		-g -c -fPIC \
+		-I$ODLA_INC \
+		-o $outputPath/$model.o
+
+	# generate so 
+	g++ $outputPath/$model.o $outputPath/$model.bin \
+		-shared \
+		-lodla_tensorrt \
+		-g \
+		-Wl,-rpath=$ODLA_LIB \
+		-L $ODLA_LIB  \
+		-o $outputPath/$model.so
+	
+	cd $cntPath
+}
+
+setEnvVar
+checkOutput
+compileModelWithHalo
+python3 detection.py
