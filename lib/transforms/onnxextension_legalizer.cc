697ddc5b39636a5bbd15843727f9d7f1bd3ed98c
Ajiang <106289713+hjweiAli@users.noreply.github.com>
2022-05-26 14:21:05 +0800

    Update onnxextension_legalizer.cc

--- lib/transforms/onnxextension_legalizer.cc
+++ lib/transforms/onnxextension_legalizer.cc
@@ -362,25 +362,24 @@ static std::vector<Def> ConvertSum(const ONNXExtensionInst* ext,
                                    IRBuilder* builder) {
   // Conver to a chain of adds.
   auto n = ext->GetNumOfOperands();
-  auto input = ext->GetOperand(0);
   HLCHECK(n >= 1);
-  switch(n)
-  {
-    case 1:{
+  auto input = ext->GetOperand(0);
+  switch (n) {
+    case 1:
+
       return {input};
       break;
-    }
+
     default:
-      {
-      auto op0 = builder->CreateAdd(ext->GetName(), input,
-                                    ext->GetOperand(1));
+
+      auto op0 = builder->CreateAdd(ext->GetName(), input, ext->GetOperand(1));
       for (unsigned i = 2; i < n; ++i) {
         op0 = builder->CreateAdd(ext->GetName() + std::to_string(i - 1), *op0,
                                  ext->GetOperand(i));
       }
+
       return {*op0};
       break;
-      }
   }
 }
 static std::vector<Def> ConvertFlatten(const ONNXExtensionInst* ext,
