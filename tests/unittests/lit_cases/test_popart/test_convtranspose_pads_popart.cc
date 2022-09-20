7bf92fa016b25cab93ec57849cc89d74dfea0b16
Weiming Zhao <weiming.zhao@alibaba-inc.com>
2022-09-19 23:13:45 -0700

    Fix for n-dim pooling and deconv

--- tests/unittests/lit_cases/test_popart/test_convtranspose_pads_popart.cc
+++ tests/unittests/lit_cases/test_popart/test_convtranspose_pads_popart.cc
@@ -26,5 +26,5 @@
 // RUN: %t_popart.exe 0.0001 0 popart %data_path/test_convtranspose_pads | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_convtranspose_pads_popart.cc.tmp.main.cc.in"
--- tests/unittests/lit_cases/test_tensorrt/test_averagepool_2d_ceil_tensorrt.cc
+++ tests/unittests/lit_cases/test_tensorrt/test_averagepool_2d_ceil_tensorrt.cc
@@ -25,5 +25,5 @@
 // RUN: %t_tensorrt.exe 0.0001 0 tensorrt %data_path/test_averagepool_2d_ceil | FileCheck %s
 // CHECK: Result Pass
 // clang-format on
-// XFAIL: *
+
 #include "test_averagepool_2d_ceil_tensorrt.cc.tmp.main.cc.in"
