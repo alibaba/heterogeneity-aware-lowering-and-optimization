22f58dbe9691d21ef1f39375ec9200cb3175de4b
wangcl15 <1373555614@qq.com>
2022-12-14 17:09:13 +0800

    [CI] update poplar_sdk-3.1.0 for workflow scripts.

--- .github/actions/build/build_in_docker.sh
+++ .github/actions/build/build_in_docker.sh
@@ -39,9 +39,9 @@ mkdir -p /tmp/ubuntu.cache
 extra_mnt="$extra_mnt -v /tmp/ubuntu.cache:/cache"
 
 rm -fr $MOUNT_DIR/output_ubuntu && mkdir -p $MOUNT_DIR/output_ubuntu
-extra_cmd="source /opt/poplar_sdk/poplar/enable.sh" # dummy command
-cmd="cd /build && cmake -G Ninja $cmake_flags /host/halo "
-cmd="$cmd && ninja && $extra_cmd && $check_cmds && ninja package "
+extra_cmd="source /opt/poplar_sdk/poplar/enable.sh && source /opt/poplar_sdk/popart/enable.sh"
+cmd="cd /build && $extra_cmd && cmake -G Ninja $cmake_flags /host/halo "
+cmd="$cmd && ninja && $check_cmds && ninja package "
 cmd="$cmd && cp /build/*.bz2 /host/output_ubuntu"
 docker run -e CCACHE_DIR=/cache $docker_run_flag -v $MOUNT_DIR:/host \
   --tmpfs /build:exec --tmpfs /tmp:exec --entrypoint="" \
